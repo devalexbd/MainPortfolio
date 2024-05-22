2a63adeed248198db1e260d7551696830fe3ecc7
ABDillustration <alexbdillustration@gmail.com>
2024-05-22 16:45:02 +0100

    Major UI Update
    Adding new transition attributes to CSS button elements. Discarding the iungo.html page in favour for a dropdown menu to deduce clicks and pages.

--- app.js
+++ app.js
@@ -25,3 +25,40 @@ hiddenElements.forEach((el) => observer.observe(el))
 function redirect(link){
     window.location.replace(link)
 }
+
+
+
+// Changing the Header colour when scrolling
+const header = document.querySelector('header')
+const headerButton = document.getElementsByClassName('header-button')
+console.log(headerButton)
+// Creating a new div at the top of screen
+// Use if for when it goes off or on screen,
+//changes other element accordingly
+const scrollWatcher = document.createElement('div')
+
+scrollWatcher.setAttribute('data-scroll-watcher', '')
+header.before(scrollWatcher)
+
+const navObserver = new IntersectionObserver((entries) => {
+    // As it is, this observer function only applies the class when
+    // it IS intersecting with the top div.
+    // Except I want the colour to show when they are NOT intersecting
+    // , this is why "entries[0].isIntersecting" has an "!" before it
+    // E.g.
+    // if Intersect = False (not NOT intersecting), remove class
+    // if Intersect = True (NOT intersecting), add class
+    header.classList.toggle('nav-change', !entries[0].isIntersecting)
+
+    // Because "headerButton" comprises of multiple elements, using
+    // getElementsByClassName returns an Array. Therefore, if I want
+    // to apply my classList toggle to each one I must cycle through
+    // each entry in the Array. To do this, I have used a for loop:
+    for(i=0; i<headerButton.length; i++) {
+        headerButton[i].classList.toggle('header-button-change', !entries[0].isIntersecting)
+    }
+
+    // headerButton.classList.toggle('header-button-change', !entries[0].isIntersecting)
+})
+
+navObserver.observe(scrollWatcher)
