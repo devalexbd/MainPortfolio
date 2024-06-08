e423665630e6fbeae3f026142debd9885a79ed96
ABDillustration <alexbdillustration@gmail.com>
2024-06-08 10:38:19 +0100

    add_item and objects
    I created the add_item method to add new objects (that are instances of the Item Class) to the Inventory object. I created three example items to demonstrate this functionality.

--- iungo_tasks/storeClass.js
+++ iungo_tasks/storeClass.js
@@ -14,7 +14,27 @@ class Store{
     }
 
     // Creating the method to add new items to Store's inventory
-    // add_item(item){
-    //     if(item.classLis)
-    // }
+    add_item(product){
+        // if(typeof(product)== 'object'){}
+
+        if(product instanceof Item){
+            this.inventory[product.name] = product
+        } else {
+            console.log(error)
+        }
+    }
 }
+
+// Creating 3 new objects that are instances of the Item Class
+let apples = new Item('apple', 24, 150)
+let grapes = new Item('grape' , 12, 125)
+let oranges = new Item('orange', 18, 259)
+console.log(apples)
+console.log(grapes)
+console.log(oranges)
+
+let The_Shop = new Store('The Shop')
+The_Shop.add_item(apples)
+The_Shop.add_item(grapes)
+The_Shop.add_item(oranges)
+console.log(The_Shop)
