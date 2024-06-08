8316ff9a4d3625991619a4cf8d1e7f11a86335e2
ABDillustration <alexbdillustration@gmail.com>
2024-06-08 10:20:54 +0100

    Started the Classes Exercise
    Create storeClass.js and created two classes: "Item" and "Store".

---
+++ iungo_tasks/storeClass.js
@@ -0,0 +1,20 @@
+class Item {
+    // Creating constructor
+    constructor(name, price, quantity){
+        this.name = name,
+            this.price = price,
+            this.quantity = quantity
+    }
+}
+
+class Store{
+    constructor(name, inventory){
+        this.name = name,
+            this.inventory = {}
+    }
+
+    // Creating the method to add new items to Store's inventory
+    // add_item(item){
+    //     if(item.classLis)
+    // }
+}
